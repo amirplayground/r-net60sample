bf2d9862da150bbe11727f803366f0ace4f2dd1a
Amir Aliyu <hello-reussi@outlook.com>
2022-05-29 14:30:37 -0500

    build job test 06

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-#var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-#builder.Services.AddRazorPages();
+builder.Services.AddRazorPages();
 
-@var app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
